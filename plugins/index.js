c4b4eba070e157746bb7db816e4aecfb4cc4f8e0
嗷嗷叫的矮冬瓜 <9240603+aidongh@user.noreply.gitee.com>
2024-03-04 17:47:14 +0800

    toast npm依赖

---
+++ plugins/index.js
@@ -0,0 +1,18 @@
+import toast from "./my-toast.vue";
+import { createApp } from "vue";
+const Toast = (option) => {
+  const rootNode = document.createElement("div");
+  document.body.childNodes[1].appendChild(rootNode);
+  const app = createApp(toast, {
+    ...option,
+  });
+
+  return app.mount(rootNode);
+};
+Toast.install = (app) => {
+  // 注册全局属性，类似于 Vue2 的 Vue.prototype
+  app.config.globalProperties.$Toast = (options) => Toast(options).show();
+};
+Toast.show = (options) => Toast(options).show();
+
+export default Toast;
